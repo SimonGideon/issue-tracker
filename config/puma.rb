f0c5f144d3350f483e8e3843f9768fdbe1daa5c3
SimonGideon <simongideon918@gmail.com>
2025-05-28 22:57:19 +0300

    Fix puma configuration

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -10,8 +11,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
-
-allow_named_args
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
